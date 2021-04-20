f2331cf9ea6e5d2f40833da4136484b61cbb7def
Itzel Avilés <itzel_a_g@hotmail.com>
2021-04-19 23:50:27 -0600

    home and calculate size

---
+++ app/src/main/java/aviles/itzel/bioxotic/ui/calculate_size/CalculateSizeFragment.kt
@@ -0,0 +1,11 @@
+package aviles.itzel.bioxotic.ui.calculate_size
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculateSizeFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculate_size_fragment)
+    }
+}
---
+++ app/src/main/java/aviles/itzel/bioxotic/ui/species/SpeciesFragment.kt
@@ -0,0 +1,11 @@
+package aviles.itzel.bioxotic.ui.species
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpeciesFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_species_fragment)
+    }
+}
