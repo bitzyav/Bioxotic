712cd2f88f5581c963de3340a8d9b0c0279d3b49
FernandaM27 <fermirandavega@gmail.com>
2021-04-24 01:41:01 -0600

    hospital, description

---
+++ app/src/main/java/aviles/itzel/bioxotic/animal_description.kt
@@ -0,0 +1,11 @@
+package aviles.itzel.bioxotic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class animal_description : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animal_description)
+    }
+}
---
+++ app/src/main/java/aviles/itzel/bioxotic/hospital_contacts.kt
@@ -0,0 +1,11 @@
+package aviles.itzel.bioxotic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class hospital_contacts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hospital_contacts)
+    }
+}
